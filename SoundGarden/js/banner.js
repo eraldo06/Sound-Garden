82442ec45838334b4bc9d3848f00752029e96330
eraldo06 <eraldoleonardo384@gmail.com>
2022-08-17 19:16:52 -0300

    organizando arquivos

--- SoundGarden/js/banner.js
+++
@@ -1,8 +0,0 @@
-var imagens = ["img/banner-01.jpg", "img/banner-02.jpg", "img/banner-03.jpg", "img/banner-04.jpg", "img/banner-05.jpg"]
-var ImagemAtual = 0;
-
-function trocarImagem() {
-    ImagemAtual = (ImagemAtual + 1) % 5;
-    document.querySelector('.banner-principal img').src = imagens[ImagemAtual];
-}
-setInterval(trocarImagem, 1500);
